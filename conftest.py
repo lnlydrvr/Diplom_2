0c390c723676ecd95726495f168b026f82e06887
Roman Kolosov <lnlydrvr-qa@yandex.ru>
2024-10-26 02:02:32 +0300

    Изменена фикстура, написаны тесты на апдейт юзера

--- conftest.py
+++ conftest.py
@@ -6,10 +6,13 @@ import src.helpers
 @pytest.fixture(scope='function')
 def get_user_token():
     user_data = src.helpers.create_user_and_collect_user_data()
-    email, password, _ = user_data
+    email = user_data['email']
+    password = user_data['password']
+    name = user_data['name']
     payload = {
-        "email": email,
-        "password": password
+        'email': email,
+        'password': password,
+        'name': name
     }
     response = requests.post(src.data.LOGIN_USER_URL, data=payload)
     token = response.json().get('accessToken')
